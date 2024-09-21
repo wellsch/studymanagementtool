c84739f8490d5b846fe15440687038551c51889a
Charles Wells <gamesc@smcewells.com>
2024-09-20 22:16:23 -0500

    mongo practice

--- backend/basic_operations.py
+++ backend/basic_operations.py
@@ -1,10 +1,15 @@
 import datetime
 import os
 
-from dotenv import load_dontenv
+from dotenv import load_dotenv
 from pymongo import MongoClient
+from pprint import pprint
 
-load_dontenv()
+load_dotenv()
 MONGODB_URI = os.environ['MONGODB_URI']
 
-client = MongoClient()
+client = MongoClient(MONGODB_URI)
+db = client['sample_mflix']
+movies = db['movies']
+
+pprint(movies.find_one({'title': 'The Great Train Robbery'}))
