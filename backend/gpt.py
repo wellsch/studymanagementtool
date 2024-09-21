c5b9bd8ba90bc9ac5c683ad890f8fd4c1e5286a0
Abbas Shaikh <abts5280@gmail.com>
2024-09-21 14:33:50 -0500

    GPT updates

--- backend/gpt.py
+++ backend/gpt.py
@@ -7,18 +7,18 @@ import json
 load_dotenv()
 client = OpenAI(
     # Set up OpenAI API key
-    api_key=os.environ["OPENAI_API_KEY"],
+    api_key = os.environ["OPENAI_API_KEY"],
 )
 
 # ENDPOINT: Takes in a list of notes strings and a course name
 #           Returns response from chatgpt
-def generate_study_plan(course, notes):
-    prompt = build_prompt(course, notes)
+def generate_study_plan(name, notes):
+    prompt = build_prompt(name, notes)
     response = get_response(prompt)
     return json.loads(response)
 
 # Build a prompt based on the notes files
-def build_prompt(course, notes):
+def build_prompt(name, notes):
     labeled_notes = ""
     for i in range(len(notes)):
         labeled_notes += f"--note {i}--\n{notes[i]}\n\n"
@@ -41,22 +41,22 @@ Your response JSON should adhere to the following schema, representing a list of
 "agenda" is a study suggestion based on the contents of the notes. Provide both the content to study and a recommended study method.
 "time" is the expected time IN MINUTES it will take to study the suggestion.
 
-You must return each the study plans sequentially with respect to the order they appeared in the notes. Each note represents one class day and should correspond to 2-5 suggestions (PER NOTE). Here is an example response:
+You should return the study plan sequentially with respect to the order they appeared in the notes. You may have multiple notes per suggestion, or multiple suggestions per note. Here is an example response:
 
 [
     {leftbrace}
         "title": "Barriers to Rational Thinking"
         "agenda": "Review barriers to rational thinking by making flashcards for each cognitive bias. Some are harder to understand than others, so make sure you study those (like Framing Effects!)."
-        "time": 20
+        "time": 10
     {rightbrace},
     {leftbrace}
         "title": "Theories of Intelligence"
         "agenda": "Quiz yourself on different theories of intelligence. What is the difference between fluid and crystalized intelligence? What factors are linked with high/low IQ scores? Is intelligence at all genetic? Recall any studies mentioned in lecture."
-        "time": 20
+        "time": 15
     {rightbrace}
 ]
 
-Below is a list of notes from the course "{course}", delimited by '--note 1--', '--note 2--', etc. Each note is enclosed in quotations, so note 1 will look like this: \n\n--note 1--\n"[content]"\n\n You will use these to construct the JSON array of study suggestions, as detailed above.
+Below is a list of notes from the course "{name}", delimited by '--note 1--', '--note 2--', etc. Each note is enclosed in quotations, so note 1 will look like this: \n\n--note 1--\n"[content]"\n\n You will use these to construct the JSON array of study suggestions, as detailed above.
 
 {labeled_notes}
     """
