716daca4c038a29eff5095382849fbe5b0bffbe5
Charles Wells <gamesc@smcewells.com>
2024-09-21 11:15:33 -0500

    OAuth

--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -1,17 +1,35 @@
-import { Navigate } from "react-router-dom";
-import { ReactNode } from "react";
-import { useAuth } from "../auth/AuthProvider"; // Make sure to import the context
+import { useNavigate } from "react-router-dom";
+import { ReactNode, useCallback, useContext, useEffect } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 interface ProtectedRouteProps {
   children: ReactNode;
 }
 
 function ProtectedRoute({ children }: ProtectedRouteProps) {
-  const { isAuthenticated } = useAuth(); // Access the authentication state from context
+  const navigate = useNavigate();
+  const { setEmail, setUsername } = useContext(UserContext);
 
-  if (!isAuthenticated) {
-    return <Navigate to="/login" />;
-  }
+  const getUserDetails = useCallback(
+    async (accessToken: string | undefined) => {
+      const response = await fetch(
+        `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${accessToken}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setUsername(data.name);
+      setEmail(data.email);
+    },
+    [setEmail, setUsername]
+  );
+
+  useEffect(() => {
+    const accessToken = sessionStorage.getItem("token");
+    console.log(accessToken);
+
+    if (!accessToken) navigate("/login");
+    else getUserDetails(accessToken);
+  }, [getUserDetails, navigate]);
 
   return <>{children}</>;
 }
